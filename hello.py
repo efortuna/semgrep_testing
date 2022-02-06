42f1936ea280625517839b9ed3eb8c9306bbb28a
not-efortuna <41125308+not-efortuna@users.noreply.github.com>
2022-02-05 19:01:10 -0800

    added pickle stuff

--- hello.py
+++ hello.py
@@ -1,1 +1,21 @@
-print 'hello world'
+from django.http import HttpResponse
+import datetime
+
+def current_datetime(request):
+    # ok:avoid-insecure-deserialization
+    user_obj = request.cookies.get('uuid')
+    now = datetime.datetime.now()
+    html = "<html><body>It is now %s.</body></html>" % now
+
+    return "Hey there! {}!".format(pickle.loads(b64decode(html)))
+
+# pickle tests
+
+def current_datetime(request):
+    # ruleid:avoid-insecure-deserialization
+    user_obj = b64decode(request.cookies.get('uuid'))
+    now = datetime.datetime.now()
+    html = "<html><body>It is now %s.</body></html>" % now
+
+    return "Hey there! {}!".format(pickle.loads(user_obj))
+
